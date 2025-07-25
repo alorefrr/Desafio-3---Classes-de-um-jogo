7b8bc44e8d33812cada08cbbc93a4f2353d97749
alorefrr <newerthedit2@gmail.com>
2025-07-25 11:16:09 -0300

    Update index.js

--- Desafio 3 - Classes de um jogo/index.js
+++ Desafio 3 - Classes de um jogo/index.js
@@ -1,19 +1,1 @@
-// IMPORTANTE: Na DIO, as funções "gets" e "print" são acessíveis globalmente, onde:
-// - "gets": lê UMA linha com dado(s) de entrada (inputs) do usuário;
-// - "print": imprime um texto de saída (output) e pula uma linha ("\n") automaticamente.
-
-// O número de produtos a ser adicionado
-let numeroDeProdutos = parseInt(gets());
-
-// TODO: Crie um array para armazenar os produtos:
-let carrinho = [];
-
-// TODO: Leia os produtos e os adicione ao carrinho:
-for (let i = 0; i < numeroDeProdutos; i++) {
-    let produto = gets();
-    carrinho.push(produto);
-}
-
-// TODO: Exiba o carrinho final no formato esperado;
-print("Carrinho final: " + carrinho.join(", "));
 
